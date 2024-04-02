c32b0c02526f337c6b1945f80e81dd29abf2f10b
Stefano19-Bongiovanni <48969163+Stefano19-Bongiovanni@users.noreply.github.com>
2024-04-02 21:16:45 +0200

    Initial Commit

---
+++ Virtual Midi Arduino/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Virtual Midi Arduino
+//
+//  Created by Stefano Bongiovanni on 02/04/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Virtual Midi Arduino/Virtual_Midi_ArduinoApp.swift
@@ -0,0 +1,32 @@
+//
+//  Virtual_Midi_ArduinoApp.swift
+//  Virtual Midi Arduino
+//
+//  Created by Stefano Bongiovanni on 02/04/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Virtual_Midi_ArduinoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
